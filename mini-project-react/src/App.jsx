a6ece47dac37cc258bd27930f7a565ee895afcb2
Rajnish rai <rajnishrai277123@gmail.com>
2026-06-21 22:39:09 +0530

    React project

---
+++ mini-project-react/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import WeatherApp from "./WeatherApp";
+
+
+function App() {
+  
+
+  return (
+    
+     
+     <WeatherApp />
+     
+      
+  );
+}
+
+export default App
---
+++ mini-project-react/src/SearchBox.jsx
@@ -0,0 +1,52 @@
+import {useState} from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./SearchBox";
+export default function SearchBox({updateInfo}){
+     
+    let [city,setCity]=useState("");
+    
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY="65af0bfeee2839148eb68c1e030e3023";
+
+    let getWeatherInfo=async()=>{
+       let response= await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+       
+    
+    let jsonResponse=await response.json();
+    console.log(jsonResponse);
+    let result={
+        city:city,
+        temp:jsonResponse.main.temp,
+        tempMin:jsonResponse.main.temp_min,
+        tempMax:jsonResponse.main.temp_max,
+        humidity:jsonResponse.main.humidity,
+        feelsLike:jsonResponse.main.feels_Like,
+        weather:jsonResponse.weather[0].description,
+    };
+    console.log(result);
+    return result;
+};
+    
+    let handleChange=(evt)=>{
+        setCity(evt.target.value);
+    };
+    let handleSubmit=async(evt)=>{
+        evt.preventDefault();
+        console.log(city);
+        setCity("");
+       let newInfo=await getWeatherInfo();
+       updateInfo(newInfo);
+    };
+    return(
+        <div className="SearchBox">
+            
+            <form onSubmit={handleSubmit}>
+                  <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange} />
+                  <br></br>
+                  <br></br>
+                  <Button variant="contained" type="submit">Search</Button>
+            </form>
+        </div>
+    )
+}
