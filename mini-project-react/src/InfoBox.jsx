d16446b3ba4f62940dbe826e01e47e49722fa507
Rajnish rai <rajnishrai277123@gmail.com>
2026-06-22 18:25:34 +0530

    final commit

--- mini-project-react/src/InfoBox.jsx
+++ mini-project-react/src/InfoBox.jsx
@@ -8,7 +8,11 @@ import "./InfoBox.css";
 export default function InfoBox({info}){
     const INIT_URL="https://images.unsplash.com/photo-1722858343990-1604f540c15d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww";
 
-   
+   const COLD_URL="https://images.unsplash.com/photo-1453306458620-5bbef13a5bca?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZHxlbnwwfHwwfHx8MA%3D%3D";
+
+   const HOT_URL="https://images.unsplash.com/photo-1572246538688-3f326dca3951?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwc3VtbWVyfGVufDB8fDB8fHww";
+
+   const RAIN_URL="https://images.unsplash.com/photo-1620385019253-b051a26048ce?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFpbnl8ZW58MHx8MHx8fDA%3D";
     return (
         <div className="InfoBox">
             
@@ -16,7 +20,7 @@ export default function InfoBox({info}){
              <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image={INIT_URL}
+        image={info.humidity>80 ? RAIN_URL :info.temp>15 ? HOT_URL :COLD_URL}
         title="green iguana"
       />
       <CardContent>
--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -5,11 +5,16 @@ import "./SearchBox";
 export default function SearchBox({updateInfo}){
      
     let [city,setCity]=useState("");
+     let [error,setError]=useState(false);
+
     
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
     const API_KEY="65af0bfeee2839148eb68c1e030e3023";
 
     let getWeatherInfo=async()=>{
+        try{
+
+        
        let response= await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
        
     
@@ -26,17 +31,24 @@ export default function SearchBox({updateInfo}){
     };
     console.log(result);
     return result;
+}catch(err){
+   throw err;
+}
 };
     
     let handleChange=(evt)=>{
         setCity(evt.target.value);
     };
     let handleSubmit=async(evt)=>{
+        try{
         evt.preventDefault();
         console.log(city);
         setCity("");
        let newInfo=await getWeatherInfo();
        updateInfo(newInfo);
+        }catch(err){
+             setError(true);
+        }
     };
     return(
         <div className="SearchBox">
@@ -46,6 +58,7 @@ export default function SearchBox({updateInfo}){
                   <br></br>
                   <br></br>
                   <Button variant="contained" type="submit">Search</Button>
+                  {error && <p style={{color:"red"}}>No such place exists</p>}
             </form>
         </div>
     )
